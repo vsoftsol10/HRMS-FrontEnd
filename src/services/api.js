ef7c73db2d9c22079e6a06eb6231123fc80683b2
Nirmal <nirmalsep2001@gmail.com>
2025-07-07 10:48:16 +0530

    app.js updated with axios

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://hrms-backend-production-abd6.up.railway.appapi',
+  baseURL: 'https://hrms-backend-production-abd6.up.railway.app/api',
   headers: { 'Content-Type': 'application/json' }
 });
